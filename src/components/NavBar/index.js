0eab083f371622abbd9dd4f0a239702de3c5e73d
shrey <shrey.somaiya@gmail.com>
2020-11-25 19:02:03 +1100

    remove useless comment

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -31,22 +31,3 @@ const NavBar = () => {
 };
 
 export default NavBar;
-
-/*
-
-    header [ id "header" ]
-        [ div [ class "nav-container" ]
-            [ div [ class "nav-item-right-container" ]
-                [ a [ id "home", class "nav-right-item", href "/" ] [ text "Home" ]
-                , a [ id "about-button", class "nav-right-item",  href "/"] [ text "About" ]
-                , a [ class "nav-right-item", href "/projects" ] [ text "Projects." ]
-                , a [ class "nav-right-item", href "/blog" ] [ text "Blog." ]
-                , a [ class "nav-right-item", href "/resume" ] [ text "Resume." ]
-                ]
-            , a [] [ img [ onClick (ScrollTo "about-headshot"), class "nav-right-item logo", src "./src/assets/images/logo.png" ] [] ]
-            ]
-        ]
-
-
-
-*/
