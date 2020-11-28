a583bb8b12ccf10b83b3a1ad70f23dd34b0493e4
shrey <shrey.somaiya@gmail.com>
2020-11-28 19:08:21 +1100

    responsive navbar

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -1,32 +1,74 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import './index.css';
+// import './index.css';
+import { makeStyles } from '@material-ui/core/styles';
+import { Box } from '@material-ui/core';
 import logo from '../../assets/logo.svg';
 
+const useStyles = makeStyles((theme) => ({
+  header: {
+    display: 'flex',
+    justifyContent: 'flex-end',
+    paddingLeft: '10%',
+    minHeight: '9vh',
+  },
+  logo: {
+    width: '3vw',
+    height: '7vh',
+    minHeight: '30px',
+    minWidth: '30px',
+    paddingLeft: '20px !important',
+    cursor: 'pointer',
+    [theme.breakpoints.down('635')]: {
+      display: 'none',
+    },
+  },
+  navRightItem: {
+    textDecoration: 'none',
+    fontWeight: 500,
+    paddingLeft: '40px',
+    color: '#4f4f4f',
+    fontSize: '1.5rem',
+    fontFamily: 'Roboto, sans-serif',
+    [theme.breakpoints.down('635')]: {
+      // fontSize: '1rem',
+      fontSize: '1.2rem',
+      paddingLeft: '20px',
+    },
+  },
+}));
+
 const NavBar = () => {
+  const classes = useStyles();
   return (
-    <header id="header">
-      <div className="nav-container">
+    <header className={classes.header}>
+      <Box
+        display="flex"
+        alignItems="center"
+        justifyContent="space-between"
+        margin="0 5vw"
+        paddingBottom="1vh"
+      >
         <div className="nav-item-right-container">
-          <Link id="home" class="nav-right-item" to="/">
+          <Link id="home" className={classes.navRightItem} to="/">
             Home
           </Link>
           {/* this will need to be replaced */}
-          <Link id="about-button" class="nav-right-item" to="/">
+          <Link id="about-button" className={classes.navRightItem} to="/">
             About
           </Link>
-          <Link class="nav-right-item" to="/projects">
+          <Link className={classes.navRightItem} to="/projects">
             Projects
           </Link>
-          <Link class="nav-right-item" to="/blog">
+          <Link className={classes.navRightItem} to="/blog">
             Blog
           </Link>
-          <Link class="nav-right-item" to="/resume">
+          <Link className={classes.navRightItem} to="/resume">
             Resume
           </Link>
         </div>
-        <img className="logo" src={logo} alt="logo" />
-      </div>
+        <img className={classes.logo} src={logo} alt="logo" />
+      </Box>
     </header>
   );
 };
