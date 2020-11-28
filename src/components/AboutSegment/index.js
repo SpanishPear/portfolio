5a9d7045890032921394d7ca7efa8f237ed9c222
shrey <shrey.somaiya@gmail.com>
2020-11-28 18:42:55 +1100

    responsiveness with the help of MUI

--- src/components/AboutSegment/index.js
+++ src/components/AboutSegment/index.js
@@ -1,29 +1,92 @@
 import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import { Box } from '@material-ui/core';
 import headshot from '../../assets/headshot.png';
-import './index.css';
 import data from '../../assets/data.json';
 
+const useStyles = makeStyles((theme) => ({
+  aboutContainer: {
+    height: '80vh',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    border: '1px solid blue',
+  },
+
+  aboutHeadshot: {
+    height: '60vh',
+    [theme.breakpoints.down('1160')]: {
+      height: '50vw',
+      margin: 'auto 0',
+    },
+    [theme.breakpoints.down('620')]: {
+      display: 'none',
+    },
+  },
+
+  aboutContentTextContainer: {
+    display: 'flex',
+    flexDirection: 'column',
+    marginLeft: '5%',
+    width: '100%',
+  },
+
+  aboutHeadingRightBorder: {
+    width: '100%',
+    borderBottom: '5px solid  #378199',
+    marginBottom: '1vh',
+  },
+
+  aboutContentText: {
+    fontSize: '2vh',
+    width: '70%',
+    lineHeight: '3vh',
+    [theme.breakpoints.down('1415')]: {
+      width: '100%',
+    },
+    [theme.breakpoints.down('1000')]: {
+      fontSize: '0.9rem',
+    },
+    [theme.breakpoints.down('620')]: {
+      textAlign: 'center',
+    },
+  },
+
+  headingTitle: {
+    [theme.breakpoints.down('620')]: {
+      margin: '0 auto',
+    },
+  },
+}));
+
+// breaks at 825px
 const AboutSegment = () => {
-  console.log('test');
+  const classes = useStyles();
+
   return (
-    <div id="about" className="about-container">
-      <div className="about-content-container">
-        <img id="about-headshot" alt="headshot" src={headshot} />
-        <div className="about-content-text-container">
-          <div className="about-heading-container">
-            <h1 className="about-heading-text">About</h1>
-            <div className="about-heading-right-border" />
-          </div>
-          <p className="about-content-text">
+    <Box className={classes.aboutContainer}>
+      <Box display="flex" flexDirection="row" width="80%">
+        <img className={classes.aboutHeadshot} alt="headshot" src={headshot} />
+        <Box className={classes.aboutContentTextContainer}>
+          <Box display="flex" flexDirection="column" alignItems="flex-start">
+            <Box className={classes.headingTitle} m={0}>
+              <h1>About</h1>
+            </Box>
+            <div className={classes.aboutHeadingRightBorder} />
+          </Box>
+          <Box className={classes.aboutContentText}>
             <NewlineText text={data.about} />
-            If you&apos;re looking for my teaching website, you can access that{' '}
-            <a href="teach.spanishpe.ar" title="Shrey's Class">
-              here
-            </a>
-          </p>
-        </div>
-      </div>
-    </div>
+            <p>
+              If you&apos;re a student looking for my teaching website, you can
+              access that{' '}
+              <a href="teach.spanishpe.ar" title="Shrey's Class">
+                here
+              </a>
+            </p>
+          </Box>
+        </Box>
+      </Box>
+    </Box>
   );
 };
 
--- src/components/LandingSegment/index.js
+++ src/components/LandingSegment/index.js
@@ -1,14 +1,64 @@
+import { Box } from '@material-ui/core';
 import React from 'react';
-import './index.css';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme) => ({
+  landingContainer: {
+    height: '85vh',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    minHeight: '85vh',
+    border: '1px solid red',
+  },
+  landingTextTitle: {
+    fontFamily: 'Roboto, sans-serif',
+    letterSpacing: '0.1em',
+    fontWeight: 500,
+    textTransform: 'uppercase',
+    color: '#4f4f4f',
+    width: '55%',
+    fontSize: '10vw',
+    /* pretty sure I should use em/rem */
+    marginBottom: 0,
+    margin: '0 auto',
+    [theme.breakpoints.down('700')]: {
+      fontSize: '13vw',
+    },
+  },
+  landingTextSubtitle: {
+    fontFamily: 'Josefin Sans, sans-serif',
+    fontWeight: 500,
+    textTransform: 'uppercase',
+    color: '#378199',
+    fontSize: '3.5vw',
+    /* pretty sure I should use em/rem */
+    marginTop: 0,
+    width: '55%',
+    margin: '0 auto',
+    [theme.breakpoints.down('700')]: {
+      fontSize: '6vw',
+    },
+  },
+  textContainer: {
+    [theme.breakpoints.down('1415')]: {
+      width: '100%',
+    },
+  },
+}));
 
 const LandingSegment = () => {
+  const classes = useStyles();
+
   return (
-    <div id="landing-container">
-      <div className="home-text-container">
-        <h1 id="landing-text-title">Shrey Somaiya</h1>
-        <h3 className="landing-text-subtitle">Computer Scientist</h3>
-      </div>
-    </div>
+    <Box className={classes.landingContainer}>
+      <Box display="flex" flexDirection="column">
+        <Box className={classes.textContainer}>
+          <h1 className={classes.landingTextTitle}>Shrey Somaiya</h1>
+          <h3 className={classes.landingTextSubtitle}>Computer Scientist</h3>
+        </Box>
+      </Box>
+    </Box>
   );
 };
 
